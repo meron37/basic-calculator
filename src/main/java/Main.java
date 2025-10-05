bd7631fd1aa467060815ad0ac1a922e5b6bd3954
meron37 <134938986+meron37@users.noreply.github.com>
2025-10-05 16:15:24 -0500

    I have changed on main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,33 +1,30 @@
 
 
-import java.util.Scanner;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
         Scanner myScanner = new Scanner(System.in);
 
-        // Ask for first number
-        System.out.println("Enter the first number:");
-        double firstNumber = myScanner.nextDouble();
+        System.out.print("Enter num1: ");
+      float num1 = myScanner.nextFloat();
+        System.out.print("Enter num2: ");
+      float num2 = myScanner.nextFloat();
+      // Ask user if they want to add, subtract,multiply or divide.
+        System.out.println("Possible calculations:");
+        System.out.println("(A) add");
+        System.out.println("(B) subtract");
+        System.out.println("(C) multiply");
+        System.out.println("(D) divide");
 
-        // Ask for second number
-        System.out.println("Enter the second number:");
-        double secondNumber = myScanner.nextDouble();
 
-        // Show calculation options
-        System.out.println("\nPossible calculations:");
-        System.out.println("(A)dd");
-        System.out.println("(S)ubtract");
-        System.out.println("(M)ultiply");
-        System.out.println("(D)ivide");
-
+//        System.out.println(num1 * num2);
         System.out.print("Please select an option: ");
         String option = myScanner.next();  // Read the option
 
         // For this exercise: always multiply
-        double result = firstNumber * secondNumber;
-        System.out.println("\n" + firstNumber + " * " + secondNumber + " = " + result);
+        double result = num1 * num2;
+        System.out.println("\n" + num1 + " * " + num2 + " = " + result);
 
-        myScanner.close();
     }
 }
